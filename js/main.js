0dee76ee40f7e221aa7f8084b8c6c567d343b8df
IlseMagali <ilsemke@gmail.com>
2017-07-05 22:26:56 +0000

    header por mejorar

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Plugin to initialization modal
+ $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
