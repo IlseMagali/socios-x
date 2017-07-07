83a72ddd109add26ba36d727b80b601419973ef4
IlseMagali <ilsemke@gmail.com>
2017-07-07 17:29:32 +0000

    menu hamburguesa y header adaptado, scripts en views

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
+$('.button-collapse').sideNav({
+ menuWidth: 300, // Default is 300
+ edge: 'right', // Choose the horizontal origin
+ closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+ draggable: true // Choose whether you can drag to open on touch screens
+});
+
+var cargarProgressBar = function() {
+ var scroll = document.body.scrollTop;
+ if (scroll >= 600) {
+   $('.progress').show('slow');
+ } else if (scroll < 599) {
+   $('.progress').hide();
+ }
+};
+
 $(document).ready(function(){
     $('.modal').modal();
     $('.collapsible').collapsible();
   });
-
-
---
+++ readme.md
@@ -0,0 +1,5 @@
+# MICROSITIO EXPERIMENTA - SOCIOS
+El proyecto para este hackatón consiste en desarrollar un micrositio (plataforma) que permita a los socios visualizar, editar o actualizar la información referente a las experiencias que ofrece por medio de la empresa Experimenta.
+
+> Definición y desarrollo del proyecto:
+https://drive.google.com/drive/folders/0B8tJ-Ii0smzHSDJKeTFfUi1KelU
